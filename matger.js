03c5faeb58708a09adb8e3cf31d75a2d0346b883
MinaMohsen466 <105835285+MinaMohsen466@users.noreply.github.com>
2022-05-27 05:14:16 +0200

    update matger project by added clothes page and nav bar

--- matger.js
+++ matger.js
@@ -1,7 +1,17 @@
 var bar = document.getElementById('fa-bars'),
-    bardiv = document.getElementById('active');
+    bardiv = document.getElementById('active'),
+    marge = document.getElementsByClassName("marg");
 
 bar.onclick = function(){
     bardiv.classList.toggle('active');
     bar.classList.toggle ('fa-xmark');
 };    
+window.onscroll = function(){
+    var value = scrollY;
+    if(value >= 50){
+        bar.style.color = "#b3f902";
+    }
+    else if(value <= 50){
+        bar.style.color = "white";
+    }
+};
