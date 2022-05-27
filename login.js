ae6f744ecba87ffb38d20fa44a22fc29af5a1832
MinaMohsen466 <105835285+MinaMohsen466@users.noreply.github.com>
2022-05-27 20:04:17 +0200

    creat login page

---
+++ login.js
@@ -0,0 +1,8 @@
+var signin_button = document.getElementById("signin_btn"),
+    signin_div = document.getElementById("signin");
+
+signin_button.onclick = function(){
+
+    "use strick"
+    signin_div.classList.toggle('activeform');
+};
--- matger.js
+++ matger.js
@@ -2,11 +2,12 @@ var bar = document.getElementById('fa-bars'),
     bardiv = document.getElementById('active'),
     marge = document.getElementsByClassName("marg");
 
-bar.onclick = function(){
-    bardiv.classList.toggle('active');
-    bar.classList.toggle ('fa-xmark');
-};    
-window.onscroll = function(){
+    bar.onclick = function(){
+    bardiv.classList.toggle("active");
+    bar.classList.toggle ("fa-xmark");
+};
+
+window.onscroll=function(){
     var value = scrollY;
     if(value >= 50){
         bar.style.color = "#b3f902";
